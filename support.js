d16d5a511b88b849a4734e254c9d1cc87566797d
giaoducvietnam <giaoducvietnam29@gmail.com>
2025-10-27 13:38:30 +0700

    support

---
+++ support.js
@@ -0,0 +1,3 @@
+document.getElementById('support-chat').addEventListener('click', () => {
+  alert("Chat hỗ trợ khách hàng: Xin chào! Bạn cần giúp gì hôm nay?\n(Tính năng chat trực tuyến sẽ được kích hoạt ở bước sau)");
+});
